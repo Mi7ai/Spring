b2c30f723d2bb021ea284c2a1e778da2d6844a44
mi7ai <hottmayer@gmail.com>
2021-01-30 19:39:31 +0100

    added first bean

---
+++ src/pack1/HelloApp.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloApp {
+    public static void main(String[] args) {
+//      load spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//      retrieve bean
+        Coach c1 = context.getBean("MyCoach", Coach.class);
+//      call method
+        System.out.println(c1.getDailyWorkout());
+//      close context
+        context.close();
+
+
+    }
+}
