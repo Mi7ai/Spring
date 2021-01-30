3b07f310ef2768abf465f02b286aa478c5d1dc5f
mi7ai <hottmayer@gmail.com>
2021-01-30 19:14:09 +0100

    first

---
+++ src/pack1/BaseballCoach.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class BaseballCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 min";
+	}
+}
---
+++ src/pack1/MyApp.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+//		this is hardcoded
+		Coach c1 = new TrackCoach();
+		
+		System.out.println(c1.getDailyWorkout());
+	}
+
+}
