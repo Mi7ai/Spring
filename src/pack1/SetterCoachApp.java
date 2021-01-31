879b8f4354e735ffb709a79f63fad2d40d0c2fde
mi7ai <hottmayer@gmail.com>
2021-01-31 20:18:07 +0100

    updated

--- src/pack1/SetterCoachApp.java
+++ src/pack1/SetterCoachApp.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SetterCoachApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        Coach c1 = context.getBean("Coach2", Coach.class);
+//        by accesing CricketCoach.class i have access to the class methods and interface method
+//        meanwhile if i put Coach.class i have access only yo the interface methods
+        CricketCoach c1 = context.getBean("Coach2", CricketCoach.class);
 
         System.out.println(c1.getDailyWorkout());
         System.out.println(c1.getDailyFortune());
+
 
         context.close();
 
