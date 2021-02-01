a618141a1fd3c0e9d457fceb4b3e18ff075af76a
mi7ai <hottmayer@gmail.com>
2021-02-01 12:52:27 +0100

    updated: prototype scope

---
+++ src/pack1/ScopeCoachApp.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeCoachApp {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContextScopeV2.xml");
+//        two different instances of coach
+        Coach c3 = context.getBean("Coach1", Coach.class);
+
+        Coach c4 = context.getBean("Coach1", Coach.class);
+
+        boolean res = c3 == c4;
+        System.out.println(res);
+        System.out.println("Memory location c3"+ c3);
+        System.out.println("Memory location c4"+ c4);
+    }
+}
