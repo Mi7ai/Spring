b502a6b785cbf27a47a34715c3d43de62e860935
mi7ai <hottmayer@gmail.com>
2021-02-16 20:39:39 +0100

    crm project

---
+++ web-customer-tracker/src/pack2/CustomerController.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model theModel) {
+		return "listCustomers";
+	}
+}
