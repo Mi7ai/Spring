c78dec817e04337c0e6ba7c2846fe3f91651ff35
mi7ai <hottmayer@gmail.com>
2021-02-23 17:16:55 +0100

    delete customer rest&postman

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,19 @@ public class CustomerRestController {
 		cs.saveCustomer(theCustomer);
 		
 		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer theCustomer = cs.getCustomer(customerId);
+		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found "+customerId);
+		}
+		
+		cs.deleteCustomer(customerId);
+		
+		return "Deleted customer with id "+customerId;
+		
 	}
 }
