fd644344a02a07df60492ade2781a444323b7bde
mi7ai <hottmayer@gmail.com>
2021-03-15 23:41:05 +0100

    Revert "spring security: add manual csrf token"
    This reverts commit 51b67cc481477884fe18b4dae0d8b306d00886e1.

--- spring-security-demo-04-csrf/src/main/java/com/demo/config/SecurityWebAppInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
