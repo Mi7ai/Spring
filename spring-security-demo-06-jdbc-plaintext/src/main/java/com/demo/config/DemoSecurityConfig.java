48b60ce0589b6bbfbb71a48d72410897f1b601ef
mi7ai <hottmayer@gmail.com>
2021-03-17 23:25:02 +0100

    db auth: jdbc pom dependency

---
+++ spring-security-demo-06-jdbc-plaintext/src/main/java/com/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+//		not to worry, is just for demo purpose
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("Mike").password("ike").roles("employee"))
+		.withUser(users.username("Charlie").password("harlie").roles("employee","admin"))
+		.withUser(users.username("John").password("ohn").roles("employee","manager"))
+		;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").hasRole("employee")
+			.antMatchers("/leaders/**").hasRole("manager")
+			.antMatchers("/systems/**").hasRole("admin")
+		.and()
+		.formLogin()
+			.loginPage("/loginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/access-denied");
+	}
+
+}
