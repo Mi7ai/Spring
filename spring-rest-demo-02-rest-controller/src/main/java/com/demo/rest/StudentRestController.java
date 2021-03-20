7117be2c9d42558f1fbe967d234fbba55714d9f0
mi7ai <hottmayer@gmail.com>
2021-03-20 18:38:55 +0100

    spring-rest: get students by id api
    Hardcoded student list

--- spring-rest-demo-02-rest-controller/src/main/java/com/demo/rest/StudentRestController.java
+++ spring-rest-demo-02-rest-controller/src/main/java/com/demo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +15,25 @@ import com.demo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	private List<Student> students ;
 	
-	@GetMapping("/students")
-	public List<Student> getStudent() {
-		List<Student> students = new ArrayList<Student>();
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<Student>();
+//		hardcoded example for the moment
 		students.add(new Student("Mike","Towers"));
 		students.add(new Student("Jake","Towers"));
 		students.add(new Student("Tony","Towers"));
-		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudent() {
 		return students;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return students.get(studentId);
 	}
 }
